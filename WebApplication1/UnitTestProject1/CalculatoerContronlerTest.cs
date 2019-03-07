42ece3b1f705505c5c68cec5811f9f18729dfbca
hoangphuc1111 <caolehoangphuc1811@gmail.com>
2019-03-07 15:31:12 +0700

    test

---
+++ WebApplication1/UnitTestProject1/CalculatoerContronlerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatoerContronlerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            HomeController contronller = new HomeController();
+
+            ViewResult result = contronller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+
+        }
+        [TestMethod]
+
+        public void TestAuthor()
+        {
+            var controller = new CalculatorController();
+
+            var result = controller.ShowAuthor();
+
+            Assert.AreEqual("Cao Le Hoang Phuc", result);
+        }
+
+    }
+}
